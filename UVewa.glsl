8894dd84e9f09385bc4e0bc577aacc6fb97ee354
Lewis Saunders <lewis@lewissaunders.com>
2013-12-22 18:28:49 +0000

    Added MIT license text ot UVewa

--- UVewa.glsl
+++ UVewa.glsl
@@ -90,3 +90,13 @@ void main(void) {
 	
 	gl_FragColor = texture2DEWA(front, uvcoords);
 }
+
+
+
+// Portions of the above were adapted from the demo source code published with the paper "High Quality Elliptical Texture Filtering on GPU"
+// found at http://www.pmavridis.com/ewa.html
+// MIT license text follows:
+// Copyright (c) 2010-2011 Pavlos Mavridis
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
