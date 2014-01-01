9e729bbae1977f6b5b7648b4ca6173972da336c8
Lewis Saunders <lewis.saunders@gmail.com>
2014-01-01 20:40:49 +0000

    curved wire barely working

--- Wireless.glsl
+++ Wireless.glsl
@@ -8,28 +8,38 @@
 
 uniform sampler2D front;
 uniform float adsk_result_w, adsk_result_h;
-uniform vec2 starttrack, endtrack, startoffset, endoffset;
-uniform float radius, restoreSize, restoreMix;
+uniform vec2 starttrack, endtrack, startoffset, endoffset, bendoffset, t;
+uniform float radius, restoresize, restoremix;
 uniform bool overlay;
 
-vec2 closestPointOnLine(vec2 p0, vec2 p1, vec2 p) {
-	vec2 v = p1 - p0;
-	vec2 w = p - p0;
-	float c1 = dot(w, v);
-	float c2 = dot(v, v);
-
-	if(c1 <= 0.0)
-		return p0;
-	if(c2 <= c1)
-		return p1;
-
-	float b = c1 / c2;
-	vec2 pb = p0 + b * v;
-	return pb;
+// Return barycentric coordinates for p in triangle v1,v2,v3
+vec3 barycentrics(vec2 p, vec2 v1, vec2 v2, vec2 v3) {
+	float det = (v2.y-v3.y)*(v1.x-v3.x)+(v3.x-v2.x)*(v1.y-v3.y);
+	float b1 = ((v2.y-v3.y)*(p.x-v3.x)+(v3.x-v2.x)*(p.y-v3.y))/det;
+	float b2 = ((v3.y-v1.y)*(p.x-v3.x)+(v1.x-v3.x)*(p.y-v3.y))/det;
+	float b3 = 1.0 - b1 - b2;
+	return vec3(b1, b2, b3);
 }
 
-float distanceToLine(vec2 p0, vec2 p1, vec2 p) {
-	return length(closestPointOnLine(p0, p1, p) - p);
+// Return interpolated UVs for p in triangle v1,v2,v3 with vertex UVs u1,u2,u3
+vec2 uvinterp(vec2 p, vec2 v1, vec2 v2, vec2 v3, vec2 u1, vec2 u2, vec2 u3) {
+	vec3 b = barycentrics(p, v1, v2, v3);
+	return b.x*u1 + b.y*u2 + b.z*u3;
+}
+
+// Return value at p of quadratic B-spline with control points v1,v2,v3
+float quadbspline(vec2 p, vec2 v1, vec2 v2, vec2 v3) {
+	vec2 u1 = vec2(0.0, 0.0);
+	vec2 u2 = vec2(0.5, 0.0);
+	vec2 u3 = vec2(1.0, 1.0);
+	vec2 uv = uvinterp(p, v1, v2, v3, u1, u2, u3);
+	return uv.x*uv.x - uv.y;
+}
+
+// Return signed distance to zero of quadratic B-spline with control points v1,v2,v3
+float sdf(vec2 p, vec2 v1, vec2 v2, vec2 v3) {
+	float f = quadbspline(p, v1, v2, v3);
+	return f / sqrt(dFdx(f)*dFdx(f) + dFdy(f)*dFdy(f));
 }
 
 void main() {
@@ -38,11 +48,17 @@ void main() {
 
 	vec2 start = starttrack + (startoffset * res);
 	vec2 end = endtrack + (endoffset * res);
+	vec2 bend = bendoffset * res;
 
-	vec2 grad = (end - start) / length(end - start);
-	vec2 tangent = vec2(-grad.y, grad.x);
+	// Grad of the scalar field f is tangent to the wire
+	// We're taking the grad and tangent at the current pixel rather than at
+	// the closest point on the wire, which isn't totally accurate
+	float f = quadbspline(coords, start, bend, end);
+	vec2 tangent = normalize(vec2(dFdx(f), dFdy(f)));
+	vec2 grad = vec2(-tangent.y, tangent.x);
+	vec2 closest = coords - sdf(coords, start, bend, end) * tangent;
 
-	vec2 closest = closestPointOnLine(start, end, coords);
+	// Take samples from pixels on either side of the wire
 	vec2 sample1 = closest + (tangent * radius);
 	vec2 sample2 = closest - (tangent * radius);
 	vec3 color1 = texture2D(front, sample1 / res).rgb;
@@ -50,27 +66,33 @@ void main() {
 
 	vec3 o;
 	float m;
-	if(distanceToLine(start, end, coords) > radius) {
+	if(abs(sdf(coords, start, bend, end)) > radius) {
+		// Miles away from the wire, get outta here
 		o = texture2D(front, coords / res).rgb;
 		m = 0.0;
 	} else {
-		// Interpolate between the two points
+		// Interpolate between the two samples
 		float blend = length(coords - (closest + (tangent * radius)));
 		blend /= radius * 2.0;
 		o = mix(color1, color2, blend);
 
 		// Blur along the line
 		vec3 blurred = vec3(0.0);
-		float samples = 0.0;
-		for(float i = -restoreSize; i < restoreSize; i += 0.333333) {
+		float energy = 0.0;
+		for(float i = -restoresize; i < restoresize; i += 0.333333) {
 			vec2 p = coords + i * grad;
-			blurred += texture2D(front, p / res).rgb;
-			samples++;
+			float weight = 1.0 - abs(i/restoresize); // Triangular kernel, bite me
+			blurred += weight * texture2D(front, p / res).rgb;
+			energy += weight;
 		}
-		blurred /= samples;
+		if(energy == 0.0) {
+			blurred = o;
+		} else {
+			blurred /= energy;
+		}
 
-		vec3 front = texture2D(front, coords / res).rgb;
-		o += restoreMix * (front - blurred);
+		vec3 frontpix = texture2D(front, coords / res).rgb;
+		o = mix(o, o + (frontpix - blurred), restoremix);
 		m = 1.0;
 	}
 
@@ -81,9 +103,12 @@ void main() {
 		if(length(coords - end) < 3.0)
 			o = vec3(0.2, 0.8, 0.2);
 
-		if(distanceToLine(start, end, coords) < 1.0) {
-			if(mod(length(start - coords), 8.0) < 4.0)
-				o = vec3(0.4, 0.4, 0.8);
+		if(length(coords - bend) < 3.0)
+			o = vec3(0.2, 0.2, 0.8);
+
+		if(abs(sdf(coords, start, bend, end)) < 2.0) {
+				float a = smoothstep(0.0, 1.0, 1.0 - abs(sdf(coords, start, bend, end)));
+				o = mix(o, vec3(0.8, 0.4, 0.8), a);
 		}
 	}
 
