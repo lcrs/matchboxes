ca2a36e1287e4df4d60b8ad47a64f568bbab4f70
Lewis Saunders <lewis@lewissaunders.com>
2014-08-03 16:57:35 +0100

    outta control

--- Ls_Glint.glsl
+++ Ls_Glint.glsl
@@ -1,17 +1,22 @@
-const float threshold = 0.55;
-const float gain = 43.0;
-const float size = 30.0;
+const float threshold = 0.8;
+const float gain = 70.0;
+const float size = 50.0;
 const float rays = 6.0;
-const float spin = 44.0;
-const float saturation = 0.4;
-const float falloff = 0.0;
-const float aspect = 1.1;
-const float randlen = 0.9;
-const float randspin = 9.0;
-const float randgain = 0.5;
+const float spin = 49.0;
+const float falloff = 0.5;
+const float twirl = 0.0;
+const float barrel = 0.2;
+const float barrelbend = 0.0;
+const float saturation = 0.5;
+const vec3 tint = vec3(0.9, 0.95, 1.0);
+const float diffraction = 0.0;
+const float diffractionhue = 0.0;
+const float aspect = 1.0;
+const float randlen = 0.0;
+const float randspin = 0.0;
+const float randgain = 0.0;
 const float randseed = 0.1;
-const vec3 tint = vec3(1.0, 1.0, 1.0);
-const float aa = 1.0;
+const float aa = 1.5;
 #define samples (size*aa)
 #define tau (2.0*3.1415926535)
 
@@ -36,19 +41,35 @@ void main(void) {
         angle -= spin/360.0 * tau;
         angle -= rand * randspin/360.0 * tau;
         for(float i = size/samples; i < size; i += size/samples) {
+            angle -= (twirl/samples * i/size)/360.0 * tau;
             offset = i/iResolution.xy * vec2(cos(angle), sin(angle));
             offset.x *= aspect;
             offset *= abs(1.0 + randlen * rand);
+            offset -= pow((i/size), barrelbend) * barrel * (-uv+vec2(0.5, 0.5));
             sample = texture2D(iChannel0, uv + offset).rgb;
-            sample *= smoothstep(threshold, 1.0, sample) * gain;
+            sample *= smoothstep(threshold, 1.0, sample);
             sample *= abs(1.0 + randgain * rand);
             sample *= smoothstep(1.001, 1.0-falloff, i/size);
+            vec3 sampley = yuv(sample);
+            sampley.gb *= saturation;
+            float hue = tau*i/size;
+            hue -= diffractionhue/360.0 * tau;
+            vec2 rainbow = vec2(cos(hue), sin(hue)) * sampley.r;
+            sampley.gb = mix(sampley.gb, rainbow, diffraction*i/size);
+            sample = rgb(sampley);
             glint += sample;
         }
     }
     glint /= rays * samples;
+    glint *= gain;
+    glint = yuv(glint);
+    vec3 tinty = yuv(tint);
+    tinty.gb *= glint.r;
+    glint.gb = mix(glint.gb, tinty.gb, 3.0*length(tinty.gb));
+    glint = rgb(glint);
     
-    vec3 result = front + glint;
+    //vec3 result = front + glint;
+    vec3 result = 1.0 - ((1.0-front) * (1.0-glint));
 
     gl_FragColor = vec4(result, 1.0);
 }
