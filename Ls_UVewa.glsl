82707b8d7a51d6a31c00409fc563fed071f149de
Lewis Saunders <lewis.saunders@gmail.com>
2014-12-09 22:50:52 +0000

    Geared down offset control, lowered default samples

--- Ls_UVewa.glsl
+++ Ls_UVewa.glsl
@@ -94,7 +94,7 @@ void main(void) {
 	if(convertoffsetuv) {
 		uv += coords;
 	}
-	uv += offset;
+	uv += offset / 100.0;
 	vec2 uvcoords = mix(coords, uv, emult);
 
 	vec4 df = vec4(dFdx(uvcoords.x), dFdy(uvcoords.x), dFdx(uvcoords.y), dFdy(uvcoords.y));
