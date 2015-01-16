a96b5d60d759392af251f9348bc02796b39a53d9
Lewis Saunders <lewis.saunders@gmail.com>
2015-01-16 00:38:51 +0000

    lumpy basics

---
+++ Ls_Lumps.1.glsl
@@ -0,0 +1,11 @@
+// Front passthrough for later
+// lewis@lewissaunders.com
+
+uniform sampler2D front;
+uniform float adsk_result_w, adsk_result_h;
+
+void main() {
+	vec2 xy = gl_FragCoord.xy;
+	vec2 px = vec2(1.0) / vec2(adsk_result_w, adsk_result_h);
+	gl_FragColor = texture2D(front, xy * px);
+}
