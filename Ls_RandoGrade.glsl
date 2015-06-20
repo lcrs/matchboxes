23abc964960b3338bcbc938d1b5862d1144a6f0e
Lewis Saunders <lewis.saunders@gmail.com>
2015-06-20 01:36:08 +0100

    syntax n todo

--- Ls_RandoGrade.glsl
+++ Ls_RandoGrade.glsl
@@ -1,7 +1,10 @@
 // Create random grade curves
 // lewis@lewissaunders.com
 // TODO:
-//  o everything
+//  o Lock 0 and 1, lock their slopes, slope overrides = crush shadows/highs?
+//  o Prevent solarization
+//  o Luma, R, G, B, crosstalk options
+//  o HSV, Lab, YUV curve options
 
 uniform float adsk_result_w, adsk_result_h;
 uniform sampler2D front;
@@ -19,35 +22,39 @@ void main() {
 	vec2 coords = gl_FragCoord.xy / vec2(adsk_result_w, adsk_result_h);
 	vec3 frontpix = texture2D(front, coords).rgb;
 
-	// Make random control points at x,y with derivative z
+	// Make some random control points, each at (x,y) with slope z
 	for(int i = 0; i < npoints; i++) {
-		//points[i].x = rando(float(i), seed);
+		points[i].x = float(i) / float(npoints);
 		points[i].y = (float(i) / float(npoints)) + 0.2 * rando(float(i)+99.0, seed);
 		points[i].z = 2.0 * rando(float(i)+199.0, seed) - 1.0;
-
-		points[i].x = float(i) / float(npoints);
-		//points[i].y = (float(i) / float(npoints)) * (float(i) / float(npoints));
-		//points[i].z = 1.0;
 	}
 
-	// Sort them by x
-	/*for (int j = 1; j < npoints; j++) {
-		int i = j - 1;
-		while(i >= 0 && points[i].x > points[j].x) {
-			points[i + 1] = points[i];
-			i--;
+	// Sort them by ascending x, naive insertion sort
+	for(int i = 1; i < npoints; i++) {
+		int j = i - 1;
+		while(j >= 0 && points[j].x > points[i].x) {
+			points[j + 1] = points[j];
+			j--;
 		}
-		points[i + 1] = points[j];
-	}*/
+		points[j + 1] = points[i];
+	}
 
-	// Evaluate spline
+	// Evaluate spline - first find which control point indices are before and after the current point
 	float x = frontpix.r;
 	int i = 1;
 	while(points[i].x < x && i < npoints - 1) i++;
-	float t = (x - points[i-1].x) / (points[i].x - points[i-1].x);
-	float a =   points[i-1].z * (points[i].x - points[i-1].x) - (points[i].y - points[i-1].y);
-	float b =  -points[i].z   * (points[i].x - points[i-1].x) + (points[i].y - points[i-1].y);
-	float f = (1.0 - t) * points[i-1].y + t * points[i].y + t * (1.0 - t) * (a * (1.0 - t) + b * t);
 
-	gl_FragColor = vec4(f);
+	// See https://en.wikipedia.org/wiki/Spline_interpolation#Algorithm_to_find_the_interpolating_cubic_spline
+	float x1 = points[i-1].x;
+	float x2 = points[i].x;
+	float y1 = points[i-1].y;
+	float y2 = points[i].y;
+	float k1 = points[i-1].z;
+	float k2 = points[i].z;
+	float t = (x - x1) / (x2 - x1);
+	float a =  k1 * (x2 - x1) - (y2 - y1);
+	float b =  -k2 * (x2 - x1) + (y2 - y1);
+	float q = (1.0 - t) * y1 + t * y2 + t * (1.0 - t) * (a * (1.0 - t) + b * t);
+
+	gl_FragColor = vec4(q);
 }
