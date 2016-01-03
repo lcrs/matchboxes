a520e9abef4a04403c9274c669fe58bc7a9bcfa3
Lewis Saunders <lewis.saunders@gmail.com>
2016-01-03 20:08:11 +0000

    Ls_Stickon 2-pass nice stuff

--- Ls_Stickon.1.glsl
+++ Ls_Stickon.1.glsl
@@ -1,12 +1,11 @@
 // Recursively warp an image by motion vectors to stick it to something
-// TODO:
-//  o Matte passthrough doesn't work :( :(
 // lewis@lewissaunders.com
+// Pass 1: recursively distort image or UV map by the motion vectors
 
-uniform sampler2D front, vectors, matte, adsk_accum_texture;
+uniform sampler2D front, matte, vectors, adsk_accum_texture;
 uniform float adsk_result_w, adsk_result_h;
 uniform bool adsk_accum_no_prev_frame;
-uniform bool outputuvs;
+uniform bool accumulateUVs;
 
 void main() {
 	vec2 res = vec2(adsk_result_w, adsk_result_h);
@@ -15,14 +14,20 @@ void main() {
 	vec4 o;
 
 	if(adsk_accum_no_prev_frame) {
-		if(outputuvs) {
+		// We're at the first frame, about to start accumulating
+		if(accumulateUVs) {
+			// Start out with an identity UV map
 			o.rg = uv;
 			o.ba = vec2(0.0);
 		} else {
-			o = texture2D(front, uv);
+			// Start out with the first frame of the front/matte
+			o.rgb = texture2D(front, uv).rgb;
 			o.a = texture2D(matte, uv).b;
 		}
 	} else {
+		// We are mid-accumulation
+		// Take the previous output frame, offseting each sample
+		// by the vectors for this frame
 		o = texture2D(adsk_accum_texture, uv + v);
 	}
 
