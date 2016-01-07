464aa0c7243e78236a8b479a36d9530234d43f88
Lewis Saunders <lewis.saunders@gmail.com>
2016-01-07 18:24:13 +0000

    made fluid mega-simple for debugging

--- Ls_Fluid.1.glsl
+++ Ls_Fluid.1.glsl
@@ -1,58 +1,22 @@
 // Pass 1: advect the vectors by themselves
+// (modified to be super simple for debugging - this pass
+//  picks up the accumulation result and does a little colour correct)
 // lewis@lewissaunders.com
 
 uniform sampler2D start, adsk_accum_texture;
-uniform float adsk_result_w, adsk_result_h, blength, spacing, sidestep;
-uniform vec2 offset;
-uniform int samples, oversamples;
-uniform vec2 bl, tr;
+uniform float adsk_result_w, adsk_result_h;
 uniform bool adsk_accum_no_prev_frame;
-
-vec2 get(vec2 uv) {
-	if(adsk_accum_no_prev_frame){
-		return texture2D(start, uv).xy;
-	} else {
-		return texture2D(adsk_accum_texture, uv).xy;
-	}
-}
 
 void main() {
 	vec2 xy = gl_FragCoord.xy;
-
-	// Factor to convert pixels to [0,1] texture coords
 	vec2 px = vec2(1.0) / vec2(adsk_result_w, adsk_result_h);
 
-	vec2 d = get(xy * px);
-
-	float sam = float(samples);
-
-	vec3 acc = vec3(0.0);
-	for(int j = 0; j < oversamples; j++) {
-		for(int k = 0; k < oversamples; k++) {
-			// Starting point for this sample
-			xy = gl_FragCoord.xy + spacing * vec2(float(j) / (float(oversamples) + 1.0), float(k) / (float(oversamples) + 1.0));
-			float dist = 0.0;
-			// Walk along path by sampling vector image, moving, sampling, moving...
-			for(float i = 0.0; i < sam; i++) {
-				d = get(xy * px);
-				if(length(d) == 0.0) {
-					// No gradient at this point in the map, early out
-					break;
-				}
-				xy += d * (blength/sam) + blength * sidestep/1000.0 * vec2(-d.y, d.x) + (blength/32.0) * offset;
-			}
-			// Sample front image where our walk ended up
-			acc.rg += get(xy * px);
-
-			// Diffusion?
-			/*for(float ix = 0.0; ix < diffusion; ix++) {
-				for(float iy = 0.0; iy < diffusion; iy++) {
-					acc.rgb += texture2D(front, (xy + vec2(ix, iy) * dist * px)).rgb * length(vec2(ix, iy)-vec2(diffusion/2.0, diffusion/2.0)) / (diffusion * diffusion * 32.0);
-				}
-			}*/
-		}
+	vec3 acc;
+	if(adsk_accum_no_prev_frame) {
+		acc = texture2D(start, xy*px).rgb;
+	} else {
+		acc = texture2D(adsk_accum_texture, xy*px).rgb;
 	}
-	acc /= float(oversamples * oversamples);
 
-	gl_FragColor = vec4(acc, 0.0);
+	gl_FragColor = vec4(acc, 1.0);
 }
