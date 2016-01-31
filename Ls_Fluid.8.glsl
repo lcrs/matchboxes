65d8b3ec3b1c521965c315aaf6714e797d7a06aa
Lewis Saunders <lewis.saunders@gmail.com>
2016-01-31 18:23:08 +0000

    going back to fluids wip now single frame input bug is identified

--- Ls_Fluid.8.glsl
+++ Ls_Fluid.8.glsl
@@ -1,18 +1,30 @@
+// Undiverge a vector field
 // Pass 8 - combine to get pressure field
-// (modified to be super simple for debugging - this pass
-//  combines previous passes)
 // lewis@lewissaunders.com
 
 uniform sampler2D adsk_results_pass3, adsk_results_pass5, adsk_results_pass7;
 uniform float adsk_result_w, adsk_result_h;
+uniform float d1, d2, d3, amount1, amount2, amount3;
 
 void main() {
 	vec2 xy = gl_FragCoord.xy;
 	vec2 px = vec2(1.0) / vec2(adsk_result_w, adsk_result_h);
 
-	vec3 a = texture2D(adsk_results_pass3, xy * px).rgb;
-	a *= texture2D(adsk_results_pass5, xy * px).rgb;
-	a *= texture2D(adsk_results_pass7, xy * px).rgb;
+	vec3 a = vec3(0.0);
+	a.r -= amount1 * texture2D(adsk_results_pass3, (xy + vec2(+d1, 0.0)) * px).r;
+	a.r += amount1 * texture2D(adsk_results_pass3, (xy + vec2(-d1, 0.0)) * px).r;
+	a.g -= amount1 * texture2D(adsk_results_pass3, (xy + vec2(0.0, +d1)) * px).g;
+	a.g += amount1 * texture2D(adsk_results_pass3, (xy + vec2(0.0, -d1)) * px).g;
+	a.r -= amount2 * texture2D(adsk_results_pass5, (xy + vec2(+d2, 0.0)) * px).r;
+	a.r += amount2 * texture2D(adsk_results_pass5, (xy + vec2(-d2, 0.0)) * px).r;
+	a.g -= amount2 * texture2D(adsk_results_pass5, (xy + vec2(0.0, +d2)) * px).g;
+	a.g += amount2 * texture2D(adsk_results_pass5, (xy + vec2(0.0, -d2)) * px).g;
+	a.r -= amount3 * texture2D(adsk_results_pass7, (xy + vec2(+d3, 0.0)) * px).r;
+	a.r += amount3 * texture2D(adsk_results_pass7, (xy + vec2(-d3, 0.0)) * px).r;
+	a.g -= amount3 * texture2D(adsk_results_pass7, (xy + vec2(0.0, +d3)) * px).g;
+	a.g += amount3 * texture2D(adsk_results_pass7, (xy + vec2(0.0, -d3)) * px).g;
 
-	gl_FragColor = vec4(a.g);
+	float pressure = a.r + a.g;
+
+	gl_FragColor = vec4(pressure);
 }
--- Ls_Fluid.9.glsl
+++ Ls_Fluid.9.glsl
@@ -1,20 +1,26 @@
+// Undiverge a vector field
 // Pass 9 - get gradient of pressure field and subtract from original
-// (modified to be super simple for debugging - this pass
-//  combines previous passes and offsets in space a little to check
-//  if accumulation is working)
 // lewis@lewissaunders.com
 
 uniform sampler2D adsk_results_pass8, adsk_results_pass1;
 uniform float adsk_result_w, adsk_result_h;
+uniform float dpressure, amountmaster;
 
 void main() {
 	vec2 xy = gl_FragCoord.xy;
 	vec2 px = vec2(1.0) / vec2(adsk_result_w, adsk_result_h);
 
-	xy += vec2(10.0);
+	vec3 v = texture2D(adsk_results_pass1, xy * px).rgb;
 
-	vec3 a = texture2D(adsk_results_pass8, xy * px).rgb;
-	a *= texture2D(adsk_results_pass1, xy * px).rgb;
+	float pressure = texture2D(adsk_results_pass8, xy * px).g;
 
-	gl_FragColor = vec4(a, 1.0);
+	vec3 d = vec3(0.0);
+	d.r -= texture2D(adsk_results_pass8, (xy + vec2(+dpressure, 0.0)) * px).g;
+	d.r += texture2D(adsk_results_pass8, (xy + vec2(-dpressure, 0.0)) * px).g;
+	d.g -= texture2D(adsk_results_pass8, (xy + vec2(0.0, +dpressure)) * px).g;
+	d.g += texture2D(adsk_results_pass8, (xy + vec2(0.0, -dpressure)) * px).g;
+
+	vec3 undiverged = v + amountmaster * d;
+
+	gl_FragColor = vec4(undiverged, pressure);
 }
