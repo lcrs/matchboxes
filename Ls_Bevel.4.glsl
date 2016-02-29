c8e934e0edd0aac3510b168b871e7e03b63e957f
Lewis Saunders <lewis.saunders@gmail.com>
2016-02-29 23:48:54 +0000

    a lot of bevelling work

--- Ls_Bevel.4.glsl
+++ Ls_Bevel.4.glsl
@@ -4,16 +4,18 @@
 
 uniform sampler2D adsk_results_pass3, adsk_results_pass1;
 uniform float adsk_result_w, adsk_result_h;
-uniform float ksize, direction, ambient;
+uniform float sigma, ambient, smoothy;
+uniform vec3 lite;
+uniform bool normals;
 
 void main() {
 	vec2 xy = gl_FragCoord.xy;
 	vec2 px = vec2(1.0) / vec2(adsk_result_w, adsk_result_h);
-
-	float sigma = 1.0;
+	float fron = texture2D(adsk_results_pass1, xy * px).g;
 
 	// Find gradients with X/Y Sobel convolution
 	vec2 d;
+	float ksize = sigma/2.0;
 	d.x  =  1.0 * texture2D(adsk_results_pass3, (xy + ksize * vec2(-1.0, -1.0)) * px).g;
 	d.x +=  2.0 * texture2D(adsk_results_pass3, (xy + ksize * vec2(-1.0,  0.0)) * px).g;
 	d.x +=  1.0 * texture2D(adsk_results_pass3, (xy + ksize * vec2(-1.0, +1.0)) * px).g;
@@ -26,11 +28,25 @@ void main() {
 	d.y += -1.0 * texture2D(adsk_results_pass3, (xy + ksize * vec2(-1.0, +1.0)) * px).g;
 	d.y += -2.0 * texture2D(adsk_results_pass3, (xy + ksize * vec2( 0.0, +1.0)) * px).g;
 	d.y += -1.0 * texture2D(adsk_results_pass3, (xy + ksize * vec2(+1.0, +1.0)) * px).g;
+	d /= ksize;
+
+	float len = length(d);
+	float edge = smoothstep(0.0, smoothy/100.0, len);
+
 	d = normalize(d);
 
-	vec4 f = texture2D(adsk_results_pass1, xy * px);
-	vec4 o = vec4(d.x) * f;
-	o = mix(o, f, ambient);
+	if(normals) {
+		vec3 n = vec3(d.x, d.y, 1.0);
+		n = mix(vec3(0.0, 0.0, 1.0), n, edge);
+		gl_FragColor = vec4(n, fron);
+		return;
+	}
 
+	float l = dot(d, normalize(lite.xy - vec2(0.5)));
+	l = mix(l, fron, ambient);
+	l = mix(ambient * fron, l, edge);
+	l *= fron;
+
+	vec4 o = vec4(l, l, l, fron);
 	gl_FragColor = o;
 }
