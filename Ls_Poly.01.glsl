b5fe6bc32bc3fc5a7137785196dbdff5f6093065
Lewis Saunders <lewis.saunders@gmail.com>
2016-08-21 17:51:05 +0100

    todo notes

--- Ls_Poly.01.glsl
+++ Ls_Poly.01.glsl
@@ -1,6 +1,23 @@
 // Poly
 // Pass 1: create seed points from input video
 
+/*
+TODO:
+o Arbitrary image sizes - hard code jump distances, probably, some jumps will read off the edge?
+o Seed generation sucks - use max of 3x3 area? Mipmaps?
+o Random colours seeded from coords
+o Outlines? Outline-only mode?
+o Shade cells radially, random angled gradients?
+o AA - don't bother, FXAA looks ok?
+o use 1+JFA instead of standard JFA?
+o Delaunay... to draw the triangles, we need to know the seed coords of all surrounding cells...
+  so we need to flood the seed coords from junction pixels
+  only need to store the triangle we're inside
+  so closest seed coord + 3 triangle coords
+  on first step: output coords of right/down/diag pixels
+  other steps: check if flooding triangles are closer than current
+*/
+
 uniform float adsk_result_w, adsk_result_h;
 uniform sampler2D front;
 uniform float seedthres;
