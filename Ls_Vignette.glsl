e4adb3885e2746a835e01e41e2015ec198944b06
Lewis Saunders <lewis.saunders@gmail.com>
2017-09-18 17:38:06 +0100

    vignette tidyup, icon, pending some MatHeMaTIcS

--- Ls_Vignette.glsl
+++ Ls_Vignette.glsl
@@ -5,8 +5,9 @@ uniform float adsk_result_w, adsk_result_h;
 uniform float adsk_result_frameratio;
 uniform vec3 edgetint, midtint;
 uniform vec2 centre;
-uniform float radius, mix, pow;
+uniform float radius, mix, abruptness, aspect;
 uniform sampler2D front;
+uniform bool clampblack, applyoffset;
 
 void main() {
   vec2 uv = gl_FragCoord.xy / vec2(adsk_result_w, adsk_result_h);
@@ -14,14 +15,28 @@ void main() {
   
   vec2 tocent = uv - centre;
   tocent.x *= adsk_result_frameratio;
+  tocent.x *= aspect;
   float rad = length(tocent);
   rad /= radius;
-  float v = pow(cos(clamp(0.0, 3.142/2.0, rad)), pow);
+  float v = pow(cos(clamp(0.0, 3.1415926535/2.0, rad)), 4.0);
+  // Do a kinda sigmoid thing for abruptness
 
-  vec3 o = f * v;
-  o = mix(o, o + (edgetint - vec3(0.5)), 1.0 - v);
-  o = mix(o, o + (midtint - vec3(0.5)), v);
+  vec3 o;
+  if(applyoffset) {
+    o = f - 0.3333 * (1.0 - v);
+    o = mix(o, o + (edgetint - vec3(0.5)), 1.0 - v);
+    o = mix(o, o + (midtint - vec3(0.5)), v);
+  } else {
+    o = f * v;
+    o = mix(o, o * (edgetint + vec3(0.5)), 1.0 - v);
+    o = mix(o, o * (midtint + vec3(0.5)), v);
+  }
+
   o = mix(f, o, mix);
+
+  if(clampblack) {
+    o = clamp(o, 0.0, 999999.0);
+  }
   
   float m = v;
   m = mix(1.0, m, mix);
