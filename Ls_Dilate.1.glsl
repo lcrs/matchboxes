afb1b0340bf7276d40c55a984dbfa29ce8b95e03
Lewis Saunders <lewis.saunders@gmail.com>
2022-10-19 00:31:55 +0100

    Ls_Dilate now erodes when size is negative, plus fixed squash/stretch ratio control

--- Ls_Dilate.1.glsl
+++ Ls_Dilate.1.glsl
@@ -1,7 +1,7 @@
 #version 120
 
 uniform sampler2D front, strength, cleanscreen;
-uniform float size, ratio;
+uniform float size, stretch;
 uniform int kernel;
 uniform bool usecolour, usecleanscreen;
 uniform vec3 screencolour;
@@ -10,44 +10,49 @@ vec2 res = vec2(adsk_result_w, adsk_result_h);
 const float pi = 3.141592653589793238462643383279502884197969;
 
 void main(void) {
-    vec2 xy = gl_FragCoord.xy;
-    vec2 sizexy = vec2(ratio, 1.0/ratio) * texture2D(strength, xy).b * size;
-	int support = int(sizexy.x * 3.0);
+	vec2 xy = gl_FragCoord.xy;
+	vec2 sizexy = mix(vec2(2.0, 0.0), vec2(0.0, 2.0), stretch) * texture2D(strength, xy).b * size;
+	int support = int(abs(sizexy.x) * 3.0);
 
 	// Centre sample
-    vec3 best = texture2D(front, xy / res).rgb + vec3(1.0);
+	vec3 best = texture2D(front, xy / res).rgb + sign(sizexy.x) * vec3(1.0);
 
 	// The rest
 	for(int i = 1; i <= support; i++) {
-        float weight = 0.0;
-        float x = float(i) / sizexy.x;
-        if(kernel == 0) {
-            // Box
-            weight = x < 1.0 ? 1.0 : 0.0;
-        } else if(kernel == 1) {
-            // Box AA
-            if(x < 1.0) {
-                weight = 1.0;
-            } else if(x < 1.0 + (1.0 / sizexy.x)) {
-                weight = 1.0 - ((x - 1.0) / (1.0 / sizexy.x));
-            } else {
-                weight = 0.0;
-            }
-        } else if(kernel == 2) {
-            // Triangle
-            weight = 1.0 - clamp(x, 0.0, 1.0);
-        } else if(kernel == 3) {
-            // Ball
-            weight = sqrt(1 - pow(clamp(x, 0.0, 1.0), 2));
-        } else if(kernel == 4) {
-            // Gaussian
-            weight = exp(-0.5 * pow(x, 2));
-        }
-		vec3 a = texture2D(front, (xy - vec2(float(i), 0.0)) / res).rgb + vec3(weight);
-        best = max(best, a);
-		vec3 b = texture2D(front, (xy + vec2(float(i), 0.0)) / res).rgb + vec3(weight);
-        best = max(best, b);
+		float weight = 0.0;
+		float x = float(i) / abs(sizexy.x);
+		if(kernel == 0) {
+			// Box
+			weight = x < 1.0 ? 1.0 : 0.0;
+		} else if(kernel == 1) {
+			// Box AA
+			if(x < 1.0) {
+				weight = 1.0;
+			} else if(x < 1.0 + (1.0 / sizexy.x)) {
+				weight = 1.0 - ((x - 1.0) / (1.0 / sizexy.x));
+			} else {
+				weight = 0.0;
+			}
+		} else if(kernel == 2) {
+			// Triangle
+			weight = 1.0 - clamp(x, 0.0, 1.0);
+		} else if(kernel == 3) {
+			// Ball
+			weight = sqrt(1 - pow(clamp(x, 0.0, 1.0), 2));
+		} else if(kernel == 4) {
+			// Gaussian
+			weight = exp(-0.5 * pow(x, 2));
+		}
+		vec3 a = texture2D(front, (xy - vec2(float(i), 0.0)) / res).rgb + sign(sizexy.x) * vec3(weight);
+		vec3 b = texture2D(front, (xy + vec2(float(i), 0.0)) / res).rgb + sign(sizexy.x) * vec3(weight);
+		if(sizexy.x > 0.0) {
+			best = max(best, a);
+			best = max(best, b);
+		} else {
+			best = min(best, a);
+			best = min(best, b);
+		}
 	}
 
-    gl_FragColor = vec4(best.r, best.g, best.b, 0.0);
+	gl_FragColor = vec4(best.r, best.g, best.b, 0.0);
 }
--- Ls_Dilate.2.glsl
+++ Ls_Dilate.2.glsl
@@ -1,7 +1,7 @@
 #version 120
 
 uniform sampler2D adsk_results_pass1, strength, cleanscreen;
-uniform float size, ratio;
+uniform float size, stretch;
 uniform int kernel;
 uniform bool usecolour, usecleanscreen;
 uniform vec3 screencolour;
@@ -10,46 +10,55 @@ vec2 res = vec2(adsk_result_w, adsk_result_h);
 const float pi = 3.141592653589793238462643383279502884197969;
 
 void main(void) {
-    vec2 xy = gl_FragCoord.xy;
-    vec2 sizexy = vec2(ratio, 1.0/ratio) * texture2D(strength, xy).b * size;
-	int support = int(sizexy.y * 3.0);
+	vec2 xy = gl_FragCoord.xy;
+	vec2 sizexy = mix(vec2(2.0, 0.0), vec2(0.0, 2.0), stretch) * texture2D(strength, xy).b * size;
+	int support = int(abs(sizexy.y) * 3.0);
 
 	// Centre sample
-    vec3 best = texture2D(adsk_results_pass1, xy / res).rgb + vec3(1.0);
+	vec3 best = texture2D(adsk_results_pass1, xy / res).rgb + sign(sizexy.y) * vec3(1.0);
 
 	// The rest
-    for(int i = 1; i <= support; i++) {
-        float weight = 0.0;
-        float x = float(i) / sizexy.y;
-        if(kernel == 0) {
-            // Box
-            weight = x < 1.0 ? 1.0 : 0.0;
-        } else if(kernel == 1) {
-            // Box AA
-            if(x < 1.0) {
-                weight = 1.0;
-            } else if(x < 1.0 + (1.0 / sizexy.y)) {
-                weight = 1.0 - ((x - 1.0) / (1.0 / sizexy.y));
-            } else {
-                weight = 0.0;
-            }
-        } else if(kernel == 2) {
-            // Triangle
-            weight = 1.0 - clamp(x, 0.0, 1.0);
-        } else if(kernel == 3) {
-            // Ball
-            weight = sqrt(1 - pow(clamp(x, 0.0, 1.0), 2));
-        } else if(kernel == 4) {
-            // Gaussian
-            weight = exp(-0.5 * pow(x, 2));
-        }
-        vec3 a = texture2D(adsk_results_pass1, (xy - vec2(0.0, float(i))) / res).rgb + vec3(weight);
-        best = max(best, a);
-        vec3 b = texture2D(adsk_results_pass1, (xy + vec2(0.0, float(i))) / res).rgb + vec3(weight);
-        best = max(best, b);
-    }
+	for(int i = 1; i <= support; i++) {
+		float weight = 0.0;
+		float x = float(i) / abs(sizexy.y);
+		if(kernel == 0) {
+			// Box
+			weight = x < 1.0 ? 1.0 : 0.0;
+		} else if(kernel == 1) {
+			// Box AA
+			if(x < 1.0) {
+				weight = 1.0;
+			} else if(x < 1.0 + (1.0 / sizexy.y)) {
+				weight = 1.0 - ((x - 1.0) / (1.0 / sizexy.y));
+			} else {
+				weight = 0.0;
+			}
+		} else if(kernel == 2) {
+			// Triangle
+			weight = 1.0 - clamp(x, 0.0, 1.0);
+		} else if(kernel == 3) {
+			// Ball
+			weight = sqrt(1 - pow(clamp(x, 0.0, 1.0), 2));
+		} else if(kernel == 4) {
+			// Gaussian
+			weight = exp(-0.5 * pow(x, 2));
+		}
+		vec3 a = texture2D(adsk_results_pass1, (xy - vec2(0.0, float(i))) / res).rgb + sign(sizexy.y) * vec3(weight);
+		vec3 b = texture2D(adsk_results_pass1, (xy + vec2(0.0, float(i))) / res).rgb + sign(sizexy.y) * vec3(weight);
+		if(sizexy.y > 0.0) {
+			best = max(best, a);
+			best = max(best, b);
+		} else {
+			best = min(best, a);
+			best = min(best, b);
+		}
+	}
 
-    best -= vec3(2.0);
+	if(sizexy.y > 0.0) {
+		best -= vec3(2.0);
+	} else {
+		best += vec3(2.0);
+	}
 
-    gl_FragColor = vec4(best.r, best.g, best.b, 0.0);
+	gl_FragColor = vec4(best.r, best.g, best.b, 0.0);
 }
