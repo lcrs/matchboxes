1c30531202149536fb553b50e8bfb99296f10810
Lewis Saunders <lewis.saunders@gmail.com>
2022-10-19 20:52:17 +0100

    Ls_Dilate subpixel box at negative sizes fix, and plumbing for colour distance mode

--- Ls_Dilate.1.glsl
+++ Ls_Dilate.1.glsl
@@ -7,17 +7,18 @@ uniform bool usecolour, usecleanscreen;
 uniform vec3 screencolour;
 uniform float adsk_result_w, adsk_result_h;
 vec2 res = vec2(adsk_result_w, adsk_result_h);
-const float pi = 3.141592653589793238462643383279502884197969;
 
 void main(void) {
 	vec2 xy = gl_FragCoord.xy;
-	vec2 sizexy = mix(vec2(2.0, 0.0), vec2(0.0, 2.0), stretch) * texture2D(strength, xy).b * size;
-	int support = int(abs(sizexy.x) * 3.0);
+	vec2 sizexy = mix(vec2(2.0, 0.0), vec2(0.0, 2.0), stretch) * texture2D(strength, xy / res).b * size;
+	vec3 screencol = usecleanscreen ? texture2D(cleanscreen, xy / res).rgb : screencolour;
 
 	// Centre sample
 	vec3 best = texture2D(front, xy / res).rgb + sign(sizexy.x) * vec3(1.0);
+	float bestweight = 1.0;
 
 	// The rest
+	int support = int(abs(sizexy.x) * 3.0);
 	for(int i = 1; i <= support; i++) {
 		float weight = 0.0;
 		float x = float(i) / abs(sizexy.x);
@@ -28,8 +29,8 @@ void main(void) {
 			// Box AA
 			if(x < 1.0) {
 				weight = 1.0;
-			} else if(x < 1.0 + (1.0 / sizexy.x)) {
-				weight = 1.0 - ((x - 1.0) / (1.0 / sizexy.x));
+			} else if(x < 1.0 + (1.0 / abs(sizexy.x))) {
+				weight = 1.0 - ((x - 1.0) / (1.0 / abs(sizexy.x)));
 			} else {
 				weight = 0.0;
 			}
@@ -45,12 +46,37 @@ void main(void) {
 		}
 		vec3 a = texture2D(front, (xy - vec2(float(i), 0.0)) / res).rgb + sign(sizexy.x) * vec3(weight);
 		vec3 b = texture2D(front, (xy + vec2(float(i), 0.0)) / res).rgb + sign(sizexy.x) * vec3(weight);
-		if(sizexy.x > 0.0) {
-			best = max(best, a);
-			best = max(best, b);
+		if(usecolour) {
+			float bestdist = distance(best - vec3(bestweight), screencol);
+			float adist = distance(a - vec3(weight), screencol);
+			float bdist = distance(b - vec3(weight), screencol);
+			if(sizexy.x > 0.0) {
+				if(adist > bestdist) {
+					best = a;
+					bestweight = weight;
+				}
+				if(bdist > bestdist) {
+					best = b;
+					bestweight = weight;
+				}
+			} else {
+				if(adist < bestdist) {
+					best = a;
+					bestweight = weight;
+				}
+				if(bdist < bestdist) {
+					best = b;
+					bestweight = weight;
+				}
+			}
 		} else {
-			best = min(best, a);
-			best = min(best, b);
+			if(sizexy.x > 0.0) {
+				best = max(best, a);
+				best = max(best, b);
+			} else {
+				best = min(best, a);
+				best = min(best, b);
+			}
 		}
 	}
 
